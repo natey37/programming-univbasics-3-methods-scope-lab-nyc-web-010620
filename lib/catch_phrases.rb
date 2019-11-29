e40a739422c601fe04f0de5bb58d48f813b7d7a0
Nathan Brickett <natey37@gmail.com>
2019-11-29 18:26:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,11 +4,14 @@ def mario
 end
 
 def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castle"
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
 def link 
   status = "It's Dangerous To Go Alone! Take This."
   puts status
+end 
+
+def any_phrase(phrase)
 end 
